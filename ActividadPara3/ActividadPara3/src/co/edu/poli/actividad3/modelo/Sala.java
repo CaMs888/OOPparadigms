e66a7a9b37663f932f3e2474f4a26e7eb33951aa
CaMs888 <scchaves@poligran.edu.co>
2025-09-25 09:26:51 -0500

    Update Sala.java

--- ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/Sala.java
+++ ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/Sala.java
@@ -1,31 +1,36 @@
 package co.edu.poli.actividad3.modelo;
 
+/**
+ * Clase abstracta que representa la superclase Sala.
+ * Todas las salas específicas deben extender esta clase.
+ * El campo numInt funciona como identificador (ID) para CRUD.
+ */
 public abstract class Sala {
-
     private Exposicion exposicion;
     private String nombre;
-    private String numInt;
+    private String numInt; // Identificador único de la sala (ID)
 
+    /**
+     * Constructor de Sala.
+     *
+     * @param exposicion exposición asociada
+     * @param nombre nombre de la sala
+     * @param numInt identificador único (ID)
+     */
     public Sala(Exposicion exposicion, String nombre, String numInt) {
         this.exposicion = exposicion;
         this.nombre = nombre;
         this.numInt = numInt;
     }
 
+    /** Método de acceso sin parámetro (sobrecarga). */
     public String acceso() {
         return "Acceso general permitido a la sala " + this.nombre + ".";
     }
 
+    /** Método de acceso con parámetro (sobrecarga). */
     public String acceso(String tipoUsuario) {
         return "Acceso de " + tipoUsuario + " permitido a la sala " + this.nombre + ".";
-    }
-
-    public final String infoBasica() {
-        return "Sala: " + nombre + ", Número interno: " + numInt;
-    }
-
-    public Sala getSalaEspecial() {
-        return this;
     }
 
     public Exposicion getExposicion() { return exposicion; }
@@ -37,12 +42,6 @@ public abstract class Sala {
 
     @Override
     public String toString() {
-        return "Sala{" +
-                "nombre='" + nombre + '\'' +
-                ", numInt='" + numInt + '\'' +
-                ", exposicion=" + (exposicion != null ? exposicion.getNombre() : "null") +
-                '}';
+        return this.getClass().getSimpleName() + "{numInt='" + numInt + "', nombre='" + nombre + "'}";
     }
 }
-
-
