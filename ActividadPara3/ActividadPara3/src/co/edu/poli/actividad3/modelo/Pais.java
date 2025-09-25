11b918dcb4af394d1fcc4107a07618becf6fbdfb
CaMs888 <scchaves@poligran.edu.co>
2025-09-25 09:03:10 -0500

    Update Pais.java

--- ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/Pais.java
+++ ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/Pais.java
@@ -1,19 +1,30 @@
 package co.edu.poli.actividad3.modelo;
 
+/**
+ * Representa un país (atributos mínimos: código y nombre).
+ */
 public class Pais {
-
-    private final String cod_pais;  // Atributo que no se puede cambiar (final)
+    private String cod_pais;
     private String nombre;
 
+    /**
+     * Constructor de Pais.
+     *
+     * @param cod_pais código del país (ej. "CO")
+     * @param nombre nombre del país
+     */
     public Pais(String cod_pais, String nombre) {
         this.cod_pais = cod_pais;
         this.nombre = nombre;
     }
 
     public String getCod_pais() { return cod_pais; }
-
-    // No hay setter para cod_pais porque es final y no se puede cambiar
-
+    public void setCod_pais(String cod_pais) { this.cod_pais = cod_pais; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+
+    @Override
+    public String toString() {
+        return "Pais{" + "cod_pais='" + cod_pais + '\'' + ", nombre='" + nombre + '\'' + '}';
+    }
 }
