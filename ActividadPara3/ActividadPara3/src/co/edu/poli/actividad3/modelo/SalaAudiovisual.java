e6c55ffe85edaba994e8073a0dd6f2c1bf619fe9
CaMs888 <scchaves@poligran.edu.co>
2025-09-27 11:50:30 -0500

    Update SalaAudiovisual.java

--- ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/SalaAudiovisual.java
+++ ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/modelo/SalaAudiovisual.java
@@ -3,12 +3,23 @@ package co.edu.poli.actividad3.modelo;
 
 public class SalaAudiovisual extends Sala {
 
-    public SalaAudiovisual(Exposicion exposicion, String nombre, String numInt) {
+    private String equipoProyeccion;
+
+    public SalaAudiovisual(Exposicion exposicion, String nombre, String numInt, String equipoProyeccion) {
         super(exposicion, nombre, numInt);
+        this.equipoProyeccion = equipoProyeccion;
     }
 
     @Override
-    public String acceso(String tipoUsuario) {
-        return "Acceso especial audiovisual para " + tipoUsuario + " en la sala " + getNombre();
+    protected String acceso() {
+        return "Acceso general a la sala audiovisual " + getNombre() +
+               " con equipo " + equipoProyeccion;
+    }
+
+    @Override
+    protected String acceso(String tipoUsuario) {
+        return "Acceso especial audiovisual para " + tipoUsuario +
+               " en la sala " + getNombre() +
+               " con equipo " + equipoProyeccion;
     }
 }
