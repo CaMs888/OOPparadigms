0aff0be7f231c52478eca0370c43a13d3b700dcf
CaMs888 <scchaves@poligran.edu.co>
2025-09-27 12:32:07 -0500

    Update OperacionCRUD.java

--- ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/servicios/OperacionCRUD.java
+++ ActividadPara3/ActividadPara3/src/co/edu/poli/actividad3/servicios/OperacionCRUD.java
@@ -1,9 +1,10 @@
-// OperacionCRUD.java
 package co.edu.poli.actividad3.servicios;
 
+import co.edu.poli.actividad3.modelo.Sala;
+
 public interface OperacionCRUD {
-    void create(Object obj);
-    Object read(String id);
-    void update(Object obj);
-    void delete(String id);
+    boolean crear(Sala sala);
+    boolean eliminar(String numInt);
+    boolean actualizar(Sala sala);
+    Sala leer(String numInt);
 }
